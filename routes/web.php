df7ba881a026083444976703c6379e51b4c418ef
phuravongpech <phuravong17@gmail.com>
2024-04-09 23:01:39 +0700

    add html

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/product', function () {
+    return view('product');
 });
